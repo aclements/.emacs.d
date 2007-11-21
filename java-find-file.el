caea6dc926e5806067dee61a41f50dce0f05ec41
Austin Clements <amdragon@mit.edu>
2007-11-21 00:45:11 +0000

    Add a tab completing java-find-class, integrated with java-find-file via a prefix argument.
    Searching has been rewritten around a more general search-path
    mechanism that supports both tab completion and direct lookup by
    successively finding classes and subpackages with optional name
    filtering.
    
    
    git-svn-id: http://awakening.csail.mit.edu/svn/amthrax/sys/elisp@1413 ee464a63-62dc-0310-8e92-97079e79a51f

--- java-find-file.el
+++ java-find-file.el
@@ -32,7 +32,7 @@
 ;; * Be able to specify alternate source directories for other
 ;;   packages.  I'm not sure if this should just be a sourcepath or if
 ;;   it should specify which parts of the tree are where.
-;; * Completion for java-find-class
+;; * Generalize java-find-class to support unqualified names as well
 ;; * Handle static imports
 ;; * Fix up preamble parsing
 ;; * Better customization
@@ -87,6 +87,12 @@
   (save-match-data
     (split-string str "\\.")))
 
+(defun jff-unparse-class (type-name)
+  (let ((class-name (car type-name)))
+    (dolist (c (cdr type-name))
+      (setq class-name (concat class-name "." c)))
+    class-name))
+
 (defun jff-get-preamble ()
   "Parse the preamble of the current file.  The return value is a
 triple of:
@@ -99,7 +105,7 @@ there is no package declaration.
 SINGLE-TYPE-IMPORTS is a list of type names for single type
 imports (as defined by JLS 3, 7.5).
 
-TYPE-IMPORTS-ON-DEMAND is a list of type names (including the
+TYPE-IMPORTS-ON-DEMAND is a list of type names (excluding the
 trailing *) for type imports on demand.
 
 XXX This needs to be spruced up, since it's based heavily on
@@ -134,7 +140,7 @@ naming conventions and ignores sub-type imports."
             (unless (or (fourth pstate) (fifth pstate))
               (let ((pkg (jff-parse-class (match-string 1))))
                 (if (string= (match-string 2) "*")
-                    (setq star-imports (cons pkg star-imports))
+                    (setq star-imports (cons (butlast pkg) star-imports))
                   (setq imports (cons pkg imports))))))
           (list package
                 (reverse imports)
@@ -250,9 +256,9 @@ the type name as it appeared in the buffer."
 
 (defun jff-get-search-path (package)
   "Compute the search path for classes.  The result is a list of
-pairs of
+triples of
 
-  (PACKAGE PATH)
+  (search PACKAGE PATH)
 
 PACKAGE is a list of strings and PATH is a string.
 
@@ -276,55 +282,127 @@ the current source tree, but this may be extended in the future."
       (setq parts (cdr parts)
             curdir (file-name-directory
                     (directory-file-name curdir))))
-    (list (list (reverse parts) curdir))))
+    (list (list 'search (reverse parts) curdir))))
 
-(defun jff-find-qualified-class (search-path type-name maybe-nested)
+(defun jff-get-classes-and-subpackages (parents &optional name)
+  "Lookup the classes and packages contained in package.  parents
+is a search path in which to look for classes and subpackages.
+When searching for root packages and classes in the root package,
+it should be a search path as returned by `jff-get-search-path'.
+When searching for classes and subpackages contained in another
+package, it should be the result returned by a call to
+`jff-get-classes-and-subpackages' for the parent package.  If
+name is specified, then the results are restricted to the classes
+and packages whose name exactly matches the argument.
+
+Returns a list of tuples of one of the following three forms:
+
+  (search PACKAGE-POSTFIX DIRECTORY)
+  (package PACKAGE-NAME DIRECTORY)
+  (class CLASS-NAME FILE-PATH)"
+
+  (let ((dirs '())
+        (results '()))
+    ;; Find the directories we should search and the sub-search
+    ;; records
+    (dolist (parent parents)
+      (case (car parent)
+        ((search)
+         (cond ((null (second parent))
+                ;; Search this directory and drop the search record
+                (push (third parent) dirs))
+               ((or (not name)
+                    (string= (car (second parent)) name))
+                ;; We can't reify this as a search directory yet, so
+                ;; keep around the search record
+                (push (list 'search (cdr (second parent)) (third parent))
+                      results))))
+        ((package)
+         (push (third parent) dirs))
+        ((class)
+         nil)
+        (otherwise
+         (error "Unknown parent entry %s" parent))))
+    (if name
+        ;; See if a package or class of the given name exists in the
+        ;; search directories
+        (dolist (dir dirs)
+          (let* ((base-path (concat dir name))
+                 (is-pkg (file-directory-p base-path))
+                 (src-path (concat base-path ".java"))
+                 (is-src (file-exists-p src-path)))
+            (when is-pkg
+              (push (list 'package name (file-name-as-directory base-path))
+                    results))
+            (when is-src
+              (push (list 'class name src-path) results))))
+      ;; Retrieve all files from the search directories and filter
+      ;; them for packages and classes
+      (save-match-data
+        (dolist (dir dirs)
+          (let ((files (directory-files dir t)))
+            (dolist (file files)
+              (let ((basename (file-name-nondirectory file)))
+                (cond ((= (aref basename 0) ?.)
+                       ;; Ignore
+                       nil)
+                      ((file-directory-p file)
+                       (push (list 'package basename
+                                   (file-name-as-directory file))
+                             results))
+                      ((string-match "^\\(.*\\)\\.java$" basename)
+                       (push (list 'class (match-string 1 basename) file)
+                             results)))))))))
+    ;; Reverse the results, just to make them a little more friendly
+    ;; to read.
+    (nreverse results)))
+
+(defun jff-get-unique-class (search-path)
+  "Given a search path, if it contains a unique class entry,
+return the path of that class entry.  If it contains multiple
+class entries, signal an error.  If it contains no class entries,
+return nil.  This should only be used on search paths that have
+been filtered by name (otherwise the uniqueness requirement
+doesn't make sense)."
+
+  (let ((first (assq 'class search-path)))
+    (when first
+      (let* ((rest (cdr (memq first search-path)))
+             (second (assq 'class rest)))
+        (if second
+            (if (string= (second first) (second second))
+                (error "Found multiple classes called %s" (second first))
+              (error "jff-get-unique-class called on unfiltered search path"))
+          (third first))))))
+
+(defun jff-find-qualified-class (search-path type-name)
   "Search for a qualified type name in the given search path.
 search-path must be of the form returned by
 `jff-get-search-path'.  type-name must be a list of type name
-elements, some non-zero prefix of which must be a package name.
-If maybe-nested is non-nil, then the division between the package
-name and the type name is unspecified; otherwise, all but the
-last element of type-name must be a package.  Returns the
-absolute path of the source file containing the requested type,
-or nil if one cannot be found."
+elements, some possibly empty prefix of which is a package name.
+The division between the package name and the type name is
+unspecified.  Returns a pair of the absolute path of the source
+file containing the requested type and the prefix of type-name
+that corresponds to that file, or nil if one cannot be found."
 
   (catch 'done
-    (dolist (search search-path)
-      ;; Check if this package matches
-      (let ((spackage (first search))
-            (package type-name))
-        ;; Test if spackage is a prefix of package
-        (while (and spackage package (string= (car package) (car spackage)))
-          (setq spackage (cdr spackage)
-                package (cdr package)))
-        (when (null spackage)
-          ;; This element of the search path matches.  Walk down the
-          ;; directory tree, looking for packages or source files.
-          (let ((dir (second search)))
-            (while package
-              (let* ((pkg-path (concat dir (car package)))
-                     (is-pkg (if (cdr package)
-                                 (file-directory-p pkg-path)
-                               nil))
-                     (src-path (concat pkg-path ".java"))
-                     (is-src (if (or maybe-nested
-                                     (null (cdr package)))
-                                 (file-exists-p src-path)
-                               nil)))
-                ;; JLS 3, 6.5.2 prefers type names over packages
-                (cond (is-src
-                       (throw 'done src-path))
-                      (is-pkg
-                       (setq dir (file-name-as-directory pkg-path)
-                             package (cdr package)))
-                      (t
-                       (setq package nil)))))))))
+    (let ((prefix '()))
+      (dolist (tn type-name)
+        (setq prefix (nconc prefix (list tn)))
+        (setq search-path (jff-get-classes-and-subpackages search-path tn))
+        ;; If we can't search any further, give up now
+        (when (null search-path)
+          (throw 'done nil))
+        ;; JLS 3, 6.5.2 prefers type names over packages
+        (let ((cls (jff-get-unique-class search-path)))
+          (when cls
+            (throw 'done (list cls prefix))))))
+
     ;; Couldn't find it
     nil))
 
 (defun jff-find-class (type-name)
-  "Disambiguate type name and find the file it corresponds to.
+  "Disambiguate a type name and find the file it corresponds to.
 Returns a pair of the absolute path of the source file containing
 the specified type and the prefix of type-name that corresponds
 to that file, or nil if the type cannot be found.  This may also
@@ -358,47 +436,85 @@ ambiguities)."
           ;; Found it.  The class we're actually looking for may be
           ;; buried in si, if we found a nested class, so we still
           ;; have to look it up
-          (let ((res (jff-find-qualified-class search-path si t)))
+          (let ((res (jff-find-qualified-class search-path si)))
             (unless res
               ;; Since we explicitly imported this, it is an error to
               ;; not find it (unless, of course, the name got shadowed
               ;; by a local name, but that's sufficiently unlikely
               ;; that I think an error is useful)
-              (let ((class-name (car si)))
-                (dolist (c (cdr si))
-                  (setq class-name (concat class-name "." c)))
-                (error (concat "Could not find class " class-name))))
-            (throw 'done (list res (list (car type-name)))))))
+              (error (concat "Could not find class " (jff-unparse-class si))))
+            (throw 'done (list (first res) (list (car type-name)))))))
       ;; 3. Types in the same package
       ;; 4. java.lang.* (JLS 3, 7.5.5) and type imports on demand
       ;;    (ambiguity here is considered an error, but we ignore this)
       (let ((packages (cons package
                             (cons '("java" "lang")
-                                  ;; XXX Just fix jff-get-preamble
-                                  (mapcar #'butlast on-demand-imports)))))
+                                  on-demand-imports))))
         (dolist (p packages)
           (let* ((qualified (append p (list (car type-name))))
-                 (res (jff-find-qualified-class search-path qualified t)))
+                 (res (jff-find-qualified-class search-path qualified)))
             (when res
-              (throw 'done (list res (list (car type-name))))))))
+              (throw 'done (list (first res) (list (car type-name))))))))
 
-      ;; Now try assuming that successive prefixes of the name are
-      ;; qualified
-      (when (cdr type-name)
-        (let ((qualified (list (car type-name) (cadr type-name)))
-              (rest (cddr type-name)))
-          (while rest
-            (let ((res (jff-find-qualified-class search-path qualified nil)))
-              (when res
-                (throw 'done (list res qualified))))
-
-            (nconc qualified (list (car rest)))
-            (setq rest (cdr rest)))))
+      ;; Now try assuming that the name is qualified.  It might be
+      ;; that only some prefix of the name is actually a type name,
+      ;; but jff-find-qualified-class ignores everything after the
+      ;; type name, so we can use it directly.
+      (let ((res (jff-find-qualified-class search-path type-name)))
+        (when res
+          (throw 'done res)))
 
       ;; Couldn't find anything.  This isn't necessarily an error
       ;; because, for example, type name could actually have been an
       ;; expression name.
       nil)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Completion
+;;;
+
+(defun jff-get-completions (search-path package)
+  "Retrieve the completions for the given package.  Returns a
+list of strings, one for each class or subpackage contained in
+package.  The names are unqualified and the subpackage names have
+a dot appended."
+
+  (dolist (pp package)
+    (setq search-path (jff-get-classes-and-subpackages search-path pp)))
+  (mapcar (lambda (search)
+            (case (first search)
+              ((search) (concat (car (second search)) "."))
+              ((package) (concat (second search) "."))
+              ((class) (second search))))
+          (jff-get-classes-and-subpackages search-path)))
+
+(defun jff-complete-class-name (prefix predicate mode)
+  "A completion function for qualified class names."
+
+  (let* ((last-dot (let ((pos (- (length prefix) 1)))
+                     (while (and (>= pos 0)
+                                 (/= (aref prefix pos) ?.))
+                       (setq pos (- pos 1)))
+                     (and (>= pos 0) pos)))
+         (package-str (if last-dot
+                          (substring prefix 0 last-dot)
+                        ""))
+         (package (if last-dot
+                      (split-string package-str "\\.")
+                    '()))
+         (cls (if last-dot
+                  (substring prefix (+ last-dot 1))
+                prefix))
+         (subcompletions (jff-get-completions jff-completion-search-path
+                                              package))
+         (collection (if last-dot
+                         (mapcar (lambda (sc) (concat package-str "." sc))
+                                 subcompletions)
+                       subcompletions)))
+    (case mode
+      ((nil) (try-completion prefix collection predicate))
+      ((t) (all-completions prefix collection predicate))
+      ((lambda) (test-completion prefix collection predicate)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Interactive
@@ -443,13 +559,50 @@ of that class can be found, defaults to that source file."
                               (file-name-nondirectory default))))
         (find-file filename)))))
 
+(defvar java-find-class-history '())
+
 (defun java-find-class (class-name)
-  (interactive "sClass: ")
-  (find-file (jff-find-class (jff-parse-class class-name))))
+  "Find and open the specified fully qualified class.  If called
+interactively, this will prompt for the class name, providing
+conveniences such as tab completion."
+
+  (interactive
+   (let* ((preamble (jff-get-preamble))
+          (package (first preamble))
+          (jff-completion-search-path (jff-get-search-path (first preamble))))
+     (list
+      (completing-read "Find class: " #'jff-complete-class-name
+                       nil t (concat (jff-unparse-class package) ".")
+                       'java-find-class-history))))
+  ;; XXX This will have to change when we start accounting for
+  ;; imported classes.
+  (let* ((type-name (jff-parse-class class-name))
+         (preamble (jff-get-preamble))
+         (search-path (jff-get-search-path (first preamble))))
+    (dolist (tn type-name)
+      (setq search-path (jff-get-classes-and-subpackages search-path tn)))
+    (let ((cls (assq 'class search-path)))
+      (if cls
+          (find-file (third cls))
+        (error "Could not find class %s" class-name)))))
+
+(defun java-find-file (&optional arg)
+  "Like find-file, but if points is on a class name and the
+source of that class can be found, defaults to that source file.
+Alternatively, if any prefix argument is given, instead of
+prompting for a file to open, prompts for a class to open in
+standard Java type syntax.  Currently, the class must be given as
+a fully-qualified name, but should eventually support unqualified
+names."
+
+  (interactive "P")
+  (if arg
+      (call-interactively #'java-find-class)
+    (call-interactively #'java-find-file-or-class-at-point)))
 
 (eval-after-load "cc-mode"
   '(progn
      (message "Re-binding C-x C-f for java-find-file")
-     (define-key java-mode-map (kbd "\C-x\C-f") #'java-find-file-or-class-at-point)))
+     (define-key java-mode-map (kbd "\C-x\C-f") #'java-find-file)))
 
 (provide 'java-find-file)
