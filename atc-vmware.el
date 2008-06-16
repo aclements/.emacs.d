0cbd79abbb9475691990d521c642d6aa2d856571
Austin Clements <amdragon@mit.edu>
2008-06-16 21:07:09 +0000

    Highlight long lines in VMware code
    git-svn-id: http://awakening.csail.mit.edu/svn/amthrax/sys/elisp@1725 ee464a63-62dc-0310-8e92-97079e79a51f

--- atc-vmware.el
+++ atc-vmware.el
@@ -1,3 +1,12 @@
 (when (string-match "^/mts/home[0-9]/" (getenv "HOME"))
   ;; SCons scripts
-  (add-to-list 'auto-mode-alist (cons "\\.sc\\'" (function python-mode))))
+  (add-to-list 'auto-mode-alist (cons "\\.sc\\'" (function python-mode)))
+
+  ;; Highlight lines longer than 80 characters
+  (defface vmware-long-line-face
+    '((((class color)) (:background "red4")))
+    "Highlight overly long lines")
+  (font-lock-add-keywords 
+   'c-mode
+   '(("^[^\n]\\{80\\}\\(.*\\)$"
+      1 'vmware-long-line-face append))))
