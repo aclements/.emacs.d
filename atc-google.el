2fb1a4dc69b426372a16080e23621fb43c4aa1ce
Austin Clements <amdragon@mit.edu>
2009-09-05 00:22:16 +0000

    Update Google Emacs config, load go-mode, and load p4-change-mode
    git-svn-id: http://awakening.csail.mit.edu/svn/amthrax/sys/elisp@1940 ee464a63-62dc-0310-8e92-97079e79a51f

--- atc-google.el
+++ atc-google.el
@@ -2,30 +2,44 @@
 ;;; Google-specific Emacs customizations
 ;;;
 
-(when (file-accessible-directory-p "~build/public/google/")
-  ;; XXX Google has it's own outdated copy of python-mode, so grab
-  ;; mine before loading Google
-  (require 'python-mode)
-  (when (load "~build/public/google/util/google.el" t)
-    ;; Load gtags
-    (when (load "~build/nonconf/google3/tools/tags/gtags.el" t)
-      (setq google-decipher-proto-files t)
-      (global-set-key "\M-." 'google-find-tag))
-    ;; Set compile command
-    (setq compile-command "make-dbg -r")
-    ;; Load unit testing
-    (when (load (concat "~build/nonconf/google3/experimental"
-                        "/users/damonkohler/run_unit_test.el") t)
-      (global-set-key "\C-x\C-u" 'run-unit-test-for-current-buffer))
-    ;; Add perforce to magic-buffer-list
-    (setq magic-buffer-list-view-basic-info
-          (append magic-buffer-list-view-basic-info
-                  '(" "
-                    (10 p4-client
-                        :align left
-                        :trim-align right
-                        :trim ".."))))
-    ))
+(when (load "/home/build/public/eng/elisp/google.el" t)
+  (setq p4-use-p4config-exclusively t)
+
+  (require 'p4-change-mode-load)
+  (setq p4-change-diff-command '("g4" "diff")))
+
+  ;; ;; XXX Google has it's own outdated copy of python-mode, so grab
+  ;; ;; mine before loading Google
+  ;; (require 'python-mode)
+  ;; (when (load "~build/public/google/util/google.el" t)
+  ;;   ;; Load gtags
+  ;;   (when (load "~build/nonconf/google3/tools/tags/gtags.el" t)
+  ;;     (setq google-decipher-proto-files t)
+  ;;     (global-set-key "\M-." 'google-find-tag))
+  ;;   ;; Set compile command
+  ;;   (setq compile-command "make-dbg -r")
+  ;;   ;; Load unit testing
+  ;;   (when (load (concat "~build/nonconf/google3/experimental"
+  ;;                       "/users/damonkohler/run_unit_test.el") t)
+  ;;     (global-set-key "\C-x\C-u" 'run-unit-test-for-current-buffer))
+  ;;   ;; Add perforce to magic-buffer-list
+  ;;   (setq magic-buffer-list-view-basic-info
+  ;;         (append magic-buffer-list-view-basic-info
+  ;;                 '(" "
+  ;;                   (10 p4-client
+  ;;                       :align left
+  ;;                       :trim-align right
+  ;;                       :trim ".."))))
+  ;;   ))
+
+(when (load "/home/lnagy/emacs/javascript.el" t)
+  (load "/home/lnagy/emacs/go-mode.el" t)
+  (setq auto-mode-alist (append (list '("\\.go$" . go-mode))
+                                auto-mode-alist))
+  (add-hook 'go-mode-hook
+            (lambda ()
+              (setq javascript-indent-level 8
+                    indent-tabs-mode t))))
 
 (defun magic-buffer-list-getter-p4-client ()
   "Get the name of the buffer's Perforce client, or nil if there is no
