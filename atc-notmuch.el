feabbebfb8124019e8f1dc2caa6e28b3fe5bbaaa
Austin Clements <amdragon@mit.edu>
2012-05-04 22:37:13 +0000

    notmuch settings
    git-svn-id: http://awakening.csail.mit.edu/svn/amthrax/sys/elisp@2317 ee464a63-62dc-0310-8e92-97079e79a51f

---
+++ atc-notmuch.el
@@ -0,0 +1,16 @@
+(setq load-path (cons "~/sandbox/notmuch/emacs" load-path))
+(setenv "PATH" (concat "/home/amthrax/sandbox/notmuch:" (getenv "PATH")))
+(setq exec-path (cons "/home/amthrax/sandbox/notmuch" exec-path))
+(require 'notmuch)
+
+;; XXX Use bold for low-color displays and maybe only for dark
+;; backgrounds
+(setq notmuch-search-line-faces
+  '(("unread" . (:foreground "#55ff55"))
+    ("flagged" . (:foreground "#5555ff"))))
+
+(setq notmuch-search-oldest-first nil)
+
+(setq notmuch-fcc-dirs "Sent")
+
+(setq frame-background-mode 'dark)
