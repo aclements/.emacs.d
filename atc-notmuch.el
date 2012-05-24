f3da627a7bcd31898b7a9b9bc5cbe54f6aee9e05
Austin Clements <amdragon@mit.edu>
2012-05-24 06:04:36 +0000

    Only load notmuch if it exists
    git-svn-id: http://awakening.csail.mit.edu/svn/amthrax/sys/elisp@2336 ee464a63-62dc-0310-8e92-97079e79a51f

--- atc-notmuch.el
+++ atc-notmuch.el
@@ -1,16 +1,17 @@
-(setq load-path (cons "~/sandbox/notmuch/emacs" load-path))
-(setenv "PATH" (concat "/home/amthrax/sandbox/notmuch:" (getenv "PATH")))
-(setq exec-path (cons "/home/amthrax/sandbox/notmuch" exec-path))
-(require 'notmuch)
+(when (file-directory-p "~/sandbox/notmuch/emacs")
+  (setq load-path (cons "~/sandbox/notmuch/emacs" load-path))
+  (setenv "PATH" (concat "/home/amthrax/sandbox/notmuch:" (getenv "PATH")))
+  (setq exec-path (cons "/home/amthrax/sandbox/notmuch" exec-path))
+  (require 'notmuch)
 
-;; XXX Use bold for low-color displays and maybe only for dark
-;; backgrounds
-(setq notmuch-search-line-faces
-  '(("unread" . (:foreground "#55ff55"))
-    ("flagged" . (:foreground "#5555ff"))))
+  ;; XXX Use bold for low-color displays and maybe only for dark
+  ;; backgrounds
+  (setq notmuch-search-line-faces
+        '(("unread" . (:foreground "#55ff55"))
+          ("flagged" . (:foreground "#5555ff"))))
 
-(setq notmuch-search-oldest-first nil)
+  (setq notmuch-search-oldest-first nil)
 
-(setq notmuch-fcc-dirs "Sent")
+  (setq notmuch-fcc-dirs "Sent")
 
-(setq frame-background-mode 'dark)
+  (setq frame-background-mode 'dark))
