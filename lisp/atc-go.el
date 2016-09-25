a077e419d869343764e54079d127d3aba8a50cd4
Austin Clements <amdragon@mit.edu>
2016-09-25 17:01:08 -0400

    atc-go: DTRT if goimports is available but not gofmt

--- lisp/atc-go.el
+++ lisp/atc-go.el
@@ -21,10 +21,15 @@
 (eval-after-load 'go-mode
   '(progn
      (add-hook 'go-mode-hook #'atc:go-mode-setup)
-     (when (atc:want-executable "gofmt" "Add Go to your PATH")
-       (add-hook 'before-save-hook #'gofmt-before-save))
-     (when (atc:want-executable "goimports" "Run go get golang.org/x/tools/cmd/goimports")
-       (setq gofmt-command "goimports"))
+     (let* ((have-goimports
+             (atc:want-executable "goimports" "Run go get golang.org/x/tools/cmd/goimports"))
+            (have-gofmt
+             (or have-goimports
+                 (atc:want-executable "gofmt" "Add Go to your PATH"))))
+       (when have-gofmt
+         (add-hook 'before-save-hook #'gofmt-before-save))
+       (when have-goimports
+         (setq gofmt-command "goimports")))
      (font-lock-add-keywords 'go-mode '(("\\.  " 0 'trailing-whitespace t)))
      (load "~/r/go/src/golang.org/x/tools/cmd/oracle/oracle.el" t)
      (require 'go-identify)
